8377033065e762fbb8f8fcaa4fd9ae43f44fb5b4
Arsenii Bortnikov <arsbortnikov@gmail.com>
2021-04-25 15:22:43 +0300

    Create AB_initials.py

---
+++ AB_initials.py
@@ -0,0 +1,10 @@
+# name: Arsenii Bortnikov
+# fun fact: I've been living and working in Sri Lanka for more than 2 years
+
+print('  A   BBBB ')
+print(' A A  B   B')
+print('A   A B   B')
+print('AAAAA BBBB ')
+print('A   A B   B')
+print('A   A B   B')
+print('A   A BBBB ')
